84ef87a69d3e286ef9a9843c57d58fbf3e543fc1
David Gustafsson <david.gustafsson@ub.gu.se>
2022-03-25 16:08:30 +0100

    Enable sip, cronjobs, start plack and add koha background worker service

--- deploy_jobs.sh
+++ deploy_jobs.sh
@@ -11,14 +11,14 @@ fi
 printf '%s\0' \
 adjustlibris.yml \
 auto-enable-printer.yml \
-edifact.yml \
+upload-edifact.yml \ # endast prod
 fetchlibris.yml \
 gobi.yml \
 mailhog.yml \
-kvinnsam-libris2primo.yml \
+kvinnsam-libris2primo.yml \ # endast prod
 libris-oai-import.yml \
 loadlibris.yml \
-papercut.yml \
-print-notice.yml \
-primo-export.yml\
+papercut.yml \ # endast prod
+print-notice.yml \ # endast prod
+primo-export.yml\ # endast prod
 | xargs --null -I{ bash -c "echo 'RUNNING {' && ansible-playbook --vault-password-file ansible_password -i $1 {"
