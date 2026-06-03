57a478335b7dd98a61fd39a91e87534fd8922d61
David  Gustafsson <david.gustafsson@ub.gu.se>
2026-06-03 17:32:38 +0200

    Remove old unused files

--- docker/down-devel.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export KOHA_REPO=/home/stefan/work/koha/koha/Koha-devel
-
-docker-compose -f docker-compose.yml -f docker-compose.devel.yml down
--- docker/down-testbuild.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export KOHA_REPO=/home/stefan/work/koha/Koha-devel
-
-docker-compose -f docker-compose.yml -f docker-compose.testbuild.yml down
